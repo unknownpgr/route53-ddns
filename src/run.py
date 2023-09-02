e8142d1c0ed515f00871fac6795c53faa7ec56a7
unknownpgr <unknownpgr@gmail.com>
2023-09-02 18:27:41 +0900

    Fix error

--- src/run.py
+++ src/run.py
@@ -4,25 +4,25 @@ import time
 import datetime
 from urllib.request import urlopen
 
-SLEEP = 30*60
-client = boto3.client('route53')
+SLEEP = 30 * 60
+client = boto3.client("route53")
 
 
 def log(*str):
-    print(f'[{datetime.datetime.now()}]', end=' ')
+    print(f"[{datetime.datetime.now()}]", end=" ")
     print(*str)
 
 
 def get_ip():
-    return urlopen("http://checkip.amazonaws.com").read().decode('utf-8').strip()
+    return urlopen("http://checkip.amazonaws.com").read().decode("utf-8").strip()
 
 
 def update_ip(target_name, new_ip, new_ttl):
     response = client.list_hosted_zones_by_name()
-    hosted_zones = response['HostedZones']
+    hosted_zones = response["HostedZones"]
     for zone in hosted_zones:
-        name = zone['Name']
-        zone_id = zone['Id']
+        name = zone["Name"]
+        zone_id = zone["Id"]
         # First condition (.endswith) is for subdomain (e.g. www.example.com)
         # becase `name` is name of hosted zone (e.g. example.com.), It will not contain subdomain.
         # Therefore it cannot be retrieved by `name == target_name`.
@@ -34,61 +34,61 @@ def update_ip(target_name, new_ip, new_ttl):
         return
 
     response = client.list_resource_record_sets(HostedZoneId=zone_id)
-    records = response['ResourceRecordSets']
+    records = response["ResourceRecordSets"]
     for record in records:
-        name = record['Name'].replace('\\052', '*')
-        current_type = record['Type']
-        ttl = record['TTL']
-        current_value = record['ResourceRecords'][0]['Value']
+        name = record["Name"].replace("\\052", "*")
+        current_type = record["Type"]
+        ttl = record["TTL"]
+        current_value = record["ResourceRecords"][0]["Value"]
         # Second condition is also for trailing dot in domain name (e.g. example.com.)
-        if (name == target_name or name[:-1] == target_name):
-            if current_type == 'A' and current_value == new_ip and ttl == new_ttl:
-                log('IP is already up to date. Skip update.')
+        if name == target_name or name[:-1] == target_name:
+            if current_type == "A" and current_value == new_ip and ttl == new_ttl:
+                log("IP is already up to date. Skip update.")
                 return
 
     client.change_resource_record_sets(
         HostedZoneId=zone_id,
         ChangeBatch={
-            'Comment': 'string',
-            'Changes': [
+            "Comment": "string",
+            "Changes": [
                 {
-                    'Action': 'UPSERT',
-                    'ResourceRecordSet': {
-                        'Name': target_name,
-                        'Type': 'A',
-                        'TTL': new_ttl,
-                        'ResourceRecords': [
-                            {
-                                'Value': new_ip
-                            },
-                        ]
-                    }
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": target_name,
+                        "Type": "A",
+                        "TTL": new_ttl,
+                        "ResourceRecords": [
+                            {"Value": new_ip},
+                        ],
+                    },
                 },
-            ]
-        }
+            ],
+        },
     )
-    log('IP updated successfully.')
+    log("IP updated successfully.")
 
-records_to_update = os.environ['RECORD'].split(';')
-ttl_to_update = os.environ['TTL']
+
+records_to_update = os.environ["RECORD"].split(";")
+ttl_to_update = os.environ["TTL"]
 
 try:
     ttl_to_update = int(ttl_to_update)
 except:
-    log(f'Invalid TTL : {ttl_to_update}')
+    log(f"Invalid TTL : {ttl_to_update}")
     exit()
 
-log('Update folloging records:')
+log("Update folloging records:")
 for record in records_to_update:
-    log('    ', record)
+    log("    ", record)
 log()
 
 last_updated_ip = None
 while True:
     current_ip = get_ip()
     if current_ip == last_updated_ip:
+        time.sleep(SLEEP)
         continue
-    log('IP of this machine changed :', last_updated_ip, '===>', current_ip)
+    log("IP of this machine changed :", last_updated_ip, "===>", current_ip)
 
     for record_to_update in records_to_update:
         log("Record :", record_to_update)
@@ -99,10 +99,10 @@ while True:
             log("Process killed by user.")
             exit(0)
         except:
-            log('Falid to update ip.')
+            log("Falid to update ip.")
 
     try:
-        log(f'Sleep for {SLEEP}s')
+        log(f"Sleep for {SLEEP}s")
         time.sleep(SLEEP)
     except KeyboardInterrupt:
         log("Process killed by user.")
